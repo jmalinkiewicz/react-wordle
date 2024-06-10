8a65679c14b79e485003c6cf4a2096e92f0747c5
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2024-06-10 16:13:22 +0200

    add hover and tap states to stats button

--- src/App.tsx
+++ src/App.tsx
@@ -107,14 +107,27 @@ function App() {
 
   return (
     <>
-      <button
+      <motion.button
+        initial={{
+          scale: 0,
+        }}
+        animate={{
+          scale: 1,
+        }}
+        whileHover={{
+          scale: 1.05,
+        }}
+        whileTap={{
+          scale: 0.95,
+        }}
         className="absolute top-4 right-4 p-2 bg-lime-800 text-white rounded-full"
-        onClick={() => {
+        onClick={(e) => {
           setShowStats((prev) => !prev);
+          e.currentTarget.blur(); // This line removes focus from the button after clicking
         }}
       >
         {showStats ? <ChartBarsFilledIcon /> : <ChartBarsOutlineIcon />}
-      </button>
+      </motion.button>
       <main className="max-w-screen-xl m-auto mt-24">
         <h1 className="font-serif text-center text-4xl font-bold text-lime-800">
           Wordle
